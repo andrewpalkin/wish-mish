cb4b111d3e76711e601954529437dff0964660aa
VovaSv <vovasvidi@gmail.com>
2018-12-20 11:52:51 +0200

    Add semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-              Edit <code>src/App.js</code> and save to. Vova changes
-          </p>
-        </header>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
