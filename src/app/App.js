cfca22459628b4ee88c15274d9705ad762f8ea58
VovaSv <vovasvidi@gmail.com>
2018-12-25 15:00:57 +0200

    Navbar update

--- src/app/App.js
+++ src/app/App.js
@@ -34,6 +34,14 @@ class App extends Component {
           {Paragraph()}
           {Paragraph()}
           {Paragraph()}
+          {Paragraph()}
+          {Paragraph()}
+          {Paragraph()}
+          {Paragraph()}
+          {Paragraph()}
+          {Paragraph()}
+          {Paragraph()}
+          {Paragraph()}
         </div>
       </BrowserRouter>
     );
