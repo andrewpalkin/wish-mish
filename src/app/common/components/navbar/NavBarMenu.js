aa502016515730e88a3d8317f7b58f2885c39bb0
Andrew Palkin <palkin.andrew@gmail.com>
2019-03-05 19:05:52 +0200

    Account main page changes, set separate section to presented different type of data

--- src/app/common/components/navbar/NavBarMenu.js
+++ src/app/common/components/navbar/NavBarMenu.js
@@ -1,46 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Menu, Icon, Header } from "semantic-ui-react";
+import {Link} from "react-router-dom";
+import {Header, Icon, Menu, Visibility} from "semantic-ui-react";
 import styles from "../../style/style.css.js";
 
 const NavBarMenuComponent = props => {
-  return (
-    <div>
-      <Menu borderless fixed="top" style={styles.fixedMenuStyle} inverted>
-        <Menu.Item>
-          <Icon loading name="wifi" />
-        </Menu.Item>
-        <Menu.Item onClick={props.handleItemClick} name="logo">
-          <Header inverted>WISH MISH</Header>
-        </Menu.Item>
-        <Menu.Item
-          as={Link}
-          onClick={props.handleItemClick}
-          name="app"
-          exact
-          to="/app"
-          active={props.activeItem === "app"}
+    return (
+        <Visibility
+            onBottomPassed={props.setShadowMenu}
+            onBottomVisible={props.unSetShadowMenu}
+            once={false}
         >
-          Home
-        </Menu.Item>
+            <div>
+                <Menu borderless fixed="top" style={styles.fixedMenuStyle}>
+                    <Menu.Item>
+                        <Icon loading name="life ring"/>
+                    </Menu.Item>
+                    <Menu.Item onClick={props.handleItemClick} name="logo">
+                        <Header>WISH MISH</Header>
+                    </Menu.Item>
+                    <Menu.Item
+                        as={Link}
+                        onClick={props.handleItemClick}
+                        name="app"
+                        exact
+                        to="/app"
+                        active={props.activeItem === "app"}
+                    >
+                        Home
+                    </Menu.Item>
 
-        <Menu.Item
-          as={Link}
-          onClick={props.handleItemClick}
-          active={props.activeItem === "about"}
-          name="about"
-          to="/about"
-        >
-          About
-        </Menu.Item>
-
-        <Menu.Menu position="right">
-          <Menu.Item />
-          {props.children}
-        </Menu.Menu>
-      </Menu>
-    </div>
-  );
+                    <Menu.Menu position="right">
+                        <Menu.Item/>
+                        {props.children}
+                    </Menu.Menu>
+                </Menu>
+            </div>
+        </Visibility>
+    );
 };
 
 export default NavBarMenuComponent;
--- src/app/home-page/HomeComponent.js
+++ src/app/home-page/HomeComponent.js
@@ -1,116 +1,279 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import WishLightComponent from "./WishLightComponent";
 import CompleteWishComponent from "./CompleteWishComponent";
-import { Link } from "react-router-dom";
-import { Segment, Header, Grid, Button } from "semantic-ui-react";
+import {Link} from "react-router-dom";
+import {Button, Grid, Header, Menu, Segment, Image} from "semantic-ui-react";
 import dateDiff from "../common/utils/dates/dateDiff";
 
 export default class HomePage extends Component {
-  render() {
-    const currentDate = new Date();
-    return (
-      <>
-        <Segment basic>
-          <Header as="h1" textAlign="center">
-            Recently Completed Wishes
-          </Header>
-          <Header as="h3" textAlign="center" disabled>
-            These wishes were recently delivered to the happy people.
-          </Header>
-          <Grid
-            columns={3}
-            stackable
-            style={{
-              paddingTop: "20px",
-              paddingBottom: "10px"
-            }}
-          >
-            {this.props.wishesComplete &&
-              this.props.wishesComplete.map(wish => {
-                return (
-                  <Grid.Column
-                    key={wish.id}
-                    style={{
-                      paddingTop: "5px",
-                      paddingBottom: "5px",
-                      paddingRight: "5px",
-                      paddingLeft: "5px"
-                    }}
-                  >
-                    <CompleteWishComponent {...wish} key={wish.id} />
-                  </Grid.Column>
-                );
-              })}
-            <Grid.Row centered>
-              <Button
-                style={{
-                  backgroundColor: "#F89235",
-                  color: "white"
-                }}
-              >
-                See how simple to use Wish-Mish site
-              </Button>
-            </Grid.Row>
-          </Grid>
-        </Segment>
 
-        <Segment basic>
-          <Header as="h1" textAlign="center">
-            Latest Wishes
-          </Header>
-          <Header as="h3" textAlign="center" disabled>
-            These wishes were recently requested to be delivered.
-          </Header>
+    state = {activeItem: 'wisher'};
+    handleItemClick = (e, {name}) => this.setState({activeItem: name});
 
-          <Grid
-            columns={2}
-            stackable
-            style={{
-              paddingTop: "20px",
-              paddingBottom: "10px"
-            }}
-          >
-            {this.props.wishesOpen &&
-              this.props.wishesOpen.map(wish => {
-                const diffDate = dateDiff(wish.publishedDate, currentDate);
-                return (
-                  <Grid.Column
-                    key={wish.id}
-                    style={{
-                      paddingTop: "5px",
-                      paddingBottom: "5px",
-                      paddingRight: "5px",
-                      paddingLeft: "5px"
-                    }}
-                  >
-                    <Link to={"/wish/" + wish.id}>
-                      <WishLightComponent
-                        {...wish}
-                        key={wish.id}
-                        diffDate={diffDate}
-                      />
-                    </Link>
-                  </Grid.Column>
-                );
-              })}
-            <Grid.Row centered>
-              <Button
-                style={{
-                  backgroundColor: "#F89235",
-                  color: "white"
-                }}
-              >
-                See full list of wishes
-              </Button>
-            </Grid.Row>
-          </Grid>
-        </Segment>
-        <Segment basic>
-          <Header as="h1" textAlign="center">
-            How Wish-Mish works
-          </Header>
-        </Segment>
-      </>
-    );
-  }
-}
+    render() {
+        const currentDate = new Date();
+        const {activeItem} = this.state;
+
+        return (
+            <>
+                <Segment basic>
+                    <Image src='http://media.virbcdn.com/files/b1/6d1d02deff9a0674-worldmap_noborder_nopoles.png' fluid/>
+                </Segment>
+                <Segment basic>
+                    <Header as="h1" textAlign="center">
+                        Recently Completed Wishes
+                    </Header>
+                    <Header as="h3" textAlign="center" disabled>
+                        These wishes were recently delivered to the happy people.
+                    </Header>
+                    <Grid
+                        columns={3}
+                        stackable
+                        style={{
+                            paddingTop: "20px",
+                            paddingBottom: "10px"
+                        }}
+                    >
+                        {this.props.wishesComplete &&
+                        this.props.wishesComplete.map(wish => {
+                            return (
+                                <Grid.Column
+                                    key={wish.id}
+                                    style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}
+                                >
+                                    <CompleteWishComponent {...wish} key={wish.id}/>
+                                </Grid.Column>
+                            );
+                        })}
+                        <Grid.Row centered>
+                            <Button
+                                style={{
+                                    backgroundColor: "#F89235",
+                                    color: "white"
+                                }}
+                            >
+                                See how simple to use Wish-Mish site
+                            </Button>
+                        </Grid.Row>
+                    </Grid>
+                </Segment>
+
+                <Segment basic>
+                    <Header as="h1" textAlign="center">
+                        Latest Wishes
+                    </Header>
+                    <Header as="h3" textAlign="center" disabled>
+                        These wishes were recently requested to be delivered.
+                    </Header>
+
+                    <Grid
+                        columns={2}
+                        stackable
+                        style={{
+                            paddingTop: "20px",
+                            paddingBottom: "10px"
+                        }}
+                    >
+                        {this.props.wishesOpen &&
+                        this.props.wishesOpen.map(wish => {
+                            const diffDate = dateDiff(wish.publishedDate, currentDate);
+                            return (
+                                <Grid.Column
+                                    key={wish.id}
+                                    style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}
+                                >
+                                    <Link to={"/wish/" + wish.id}>
+                                        <WishLightComponent
+                                            {...wish}
+                                            key={wish.id}
+                                            diffDate={diffDate}
+                                        />
+                                    </Link>
+                                </Grid.Column>
+                            );
+                        })}
+                        <Grid.Row centered>
+                            <Button
+                                style={{
+                                    backgroundColor: "#F89235",
+                                    color: "white"
+                                }}
+                            >
+                                See full list of wishes
+                            </Button>
+                        </Grid.Row>
+                    </Grid>
+                </Segment>
+                <Segment basic>
+                    <Header as="h1" textAlign="center">
+                        How Wish-Mish works
+                    </Header>
+                    <Header as="h3" textAlign="center" disabled>
+                        Wish-Mish is the go-to way to get the products that aren’t available in your country.<p/>
+                        You can request just anything on Wish-Mish, from baby clothes and kids toys to tech gadgets.
+                    </Header>
+                    <Menu pointing widths={8} secondary>
+                        <Menu.Item name='wisher' active={activeItem === 'wisher'} onClick={this.handleItemClick}>
+                            For Wishers
+                        </Menu.Item>
+                        <Menu.Item name='traveler' active={activeItem === 'traveler'} onClick={this.handleItemClick}>
+                            For Travelers
+                        </Menu.Item>
+                    </Menu>
+                    <Segment basic>
+                        <Segment>
+                            <Grid columns={2}>
+                                <Grid.Column width={1}>
+                                    <Header as="h1" style={{
+                                        paddingTop: "15px",
+                                        paddingLeft: "15px"
+                                    }}>1</Header>
+                                </Grid.Column>
+                                <Grid.Column width={15}>
+                                    <Grid.Row style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}>
+                                        <Header as="h2">Tell us about the item you are wish to get</Header>
+                                    </Grid.Row>
+                                    <Grid.Row style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}>
+                                        <Header as="h4" disabled>
+                                            WIth Wish-Mish you can get any item from around the world. To get started,
+                                            create an wish for a product you wish and include details such as how much
+                                            it costs and from where traveler can get it.
+                                        </Header>
+                                    </Grid.Row>
+                                </Grid.Column>
+                            </Grid>
+                        </Segment>
+                        <Segment>
+                            <Grid columns={2}>
+                                <Grid.Column width={1}>
+                                    <Header as="h1" style={{
+                                        paddingTop: "15px",
+                                        paddingLeft: "15px"
+                                    }}>2</Header>
+                                </Grid.Column>
+                                <Grid.Column width={15}>
+                                    <Grid.Row style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}>
+                                        <Header as="h2">Wait for travelers to make offers</Header>
+                                    </Grid.Row>
+                                    <Grid.Row style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}>
+                                        <Header as="h4" disabled>
+                                            Once you publish your wish, we’ll share it with traveler community.
+                                            Then, travelers heading to your city will bid to deliver it by making an
+                                            offer.
+                                        </Header>
+                                    </Grid.Row>
+                                </Grid.Column>
+                            </Grid>
+                        </Segment>
+                        <Segment>
+                            <Grid columns={2}>
+                                <Grid.Column width={1}>
+                                    <Header as="h1" style={{
+                                        paddingTop: "15px",
+                                        paddingLeft: "15px"
+                                    }}>3</Header>
+                                </Grid.Column>
+                                <Grid.Column width={15}>
+                                    <Grid.Row style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}>
+                                        <Header as="h2">Agree on a reward for your traveler</Header>
+                                    </Grid.Row>
+                                    <Grid.Row style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}>
+                                        <Header as="h4" disabled>
+                                            You need to agree with your traveler about all applicable taxes and fees
+                                            (importing of item to your country),
+                                            including the monetary reward you agree to pay your traveler for delivering
+                                            your item.
+                                        </Header>
+                                    </Grid.Row>
+                                </Grid.Column>
+                            </Grid>
+                        </Segment>
+                        <Segment>
+                            <Grid columns={2}>
+                                <Grid.Column width={1}>
+                                    <Header as="h1" style={{
+                                        paddingTop: "15px",
+                                        paddingLeft: "15px"
+                                    }}>4</Header>
+                                </Grid.Column>
+                                <Grid.Column width={15}>
+                                    <Grid.Row style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}>
+                                        <Header as="h2">Meet with your traveler and receive your item</Header>
+                                    </Grid.Row>
+                                    <Grid.Row style={{
+                                        paddingTop: "5px",
+                                        paddingBottom: "5px",
+                                        paddingRight: "5px",
+                                        paddingLeft: "5px"
+                                    }}>
+                                        <Header as="h4" disabled>
+                                            Coordinate a time and place to meet your traveler. When you receive
+                                            your item, make sure to give the rating to your traveler.
+                                        </Header>
+                                    </Grid.Row>
+                                </Grid.Column>
+                            </Grid>
+                        </Segment>
+                        <Grid>
+                            <Grid.Row centered>
+                                <Button
+                                    style={{
+                                        backgroundColor: "#F89235",
+                                        color: "white"
+                                    }}
+                                >
+                                    Add new Wish now
+                                </Button>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment>
+                </Segment>
+            </>
+        );
+    };
+};
