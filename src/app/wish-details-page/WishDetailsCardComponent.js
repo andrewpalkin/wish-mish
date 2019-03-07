1a0bb914124c26759b42677cbdca7637abec2cca
Andrew Palkin <palkin.andrew@gmail.com>
2019-03-07 12:26:10 +0200

    Counter for offers on each wish

--- src/app/wish-details-page/WishDetailsCardComponent.js
+++ src/app/wish-details-page/WishDetailsCardComponent.js
@@ -1,123 +1,126 @@
 import React from "react";
-import { Button, Divider, Image, Label } from "semantic-ui-react";
+import {Button, Divider, Header, Image, Label} from "semantic-ui-react";
 
 import offerPerUserExist from "../common/utils/offerPerUserExist";
 
 const WishDetailsCardComponent = props => {
-  const wishDetails = props.wishDetails;
-  const uidOfLoggedInUser = props.uidOfLoggedInUser;
-  const offers = props.offers;
-  return (
-    <div class="ui card fluid">
-      <div class="content">
-        <div class="center aligned  author">
-          {wishDetails.imageURL ? (
-            <Image
-              circular
-              size="big"
-              src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
-            />
-          ) : (
-            <Label
-              size="big"
-              circular
-              style={{
-                marginRight: "4px",
-                backgroundColor: "#F89235",
-                color: "white"
-              }}
-            >
-              {wishDetails.initials}
-            </Label>
-          )}
-        </div>
-        <br />
+    const wishDetails = props.wishDetails;
+    const uidOfLoggedInUser = props.uidOfLoggedInUser;
+    const offers = props.offers;
+    return (
+        <div class="ui card fluid">
+            <div class="content">
+                <div class="center aligned  author">
+                    {wishDetails.imageURL ? (
+                        <Image
+                            circular
+                            size="big"
+                            src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+                        />
+                    ) : (
+                        <Label
+                            size="big"
+                            circular
+                            style={{
+                                marginRight: "4px",
+                                backgroundColor: "#F89235",
+                                color: "white"
+                            }}
+                        >
+                            {wishDetails.initials}
+                        </Label>
+                    )}
+                </div>
+                <br/>
 
-        <div class="center aligned header">{wishDetails.firstName}</div>
-        <Divider hidden />
-        <div class="meta">
-          <span class="right floated time">{`${props.diffDate} ago`}</span>
-          <span class="category">Requested</span>
-        </div>
-        <Divider />
-        <div>
+                <div class="center aligned header">{wishDetails.firstName}</div>
+                <Divider hidden/>
+                <div class="meta">
+                    <span class="right floated time">{`${props.diffDate} ago`}</span>
+                    <span class="category">Requested</span>
+                </div>
+                <Divider/>
+                <div>
           <span class="right floated time">
             <strong>{wishDetails.itemPrice}$</strong>
           </span>
-          <span class="category meta">Price for 1 item</span>
-        </div>
-        <div>
+                    <span class="category meta">Price for 1 item</span>
+                </div>
+                <div>
           <span class="right floated time">
             <strong>{wishDetails.reward}$</strong>
           </span>
-          <span class="category meta">Reward for one item</span>
-        </div>
+                    <span class="category meta">Reward for one item</span>
+                </div>
 
-        {uidOfLoggedInUser &&
-        wishDetails.userId !== uidOfLoggedInUser &&
-        !offerPerUserExist(offers, uidOfLoggedInUser) ? (
-          <>
-            <Divider hidden />
-            <Button primary fluid onClick={props.openMakeYouOfferModalStatus}>
-              Make your offer
-            </Button>
-          </>
-        ) : null}
+                {uidOfLoggedInUser &&
+                wishDetails.userId !== uidOfLoggedInUser &&
+                !offerPerUserExist(offers, uidOfLoggedInUser) ? (
+                    <>
+                        <Divider hidden/>
+                        <Button primary fluid onClick={props.openMakeYouOfferModalStatus}>
+                            Make your offer
+                        </Button>
+                    </>
+                ) : null}
 
-        <Divider />
-        <div>
+                <Divider/>
+                <div>
           <span class="right floated time">
             <strong>{wishDetails.deliveryTo}</strong>
           </span>
-          <span class="category meta">
-            <i class="share icon" />
+                    <span class="category meta">
+            <i class="share icon"/>
             Delivery to
           </span>
-        </div>
-        <div>
+                </div>
+                <div>
           <span class="right floated time">
             <strong>{wishDetails.deliveryFrom}</strong>
           </span>
-          <span class="category meta">
-            <i class="reply icon" />
+                    <span class="category meta">
+            <i class="reply icon"/>
             From
           </span>
-        </div>
-        <div>
-          <span class="right floated time">
-            <strong>{wishDetails.productURL}</strong>
-          </span>
-          <span class="category meta">
-            <i class="map pin  icon" />
+                </div>
+                <div>
+             <span className="right floated time">
+                 <Header link href={wishDetails.productURL} size="tiny" color="blue">
+                Link URL
+            </Header>
+             </span>
+
+                    <span class="category meta">
+            <i class="map pin  icon"/>
             Where to buy
           </span>
-        </div>
-        <div>
+                </div>
+                <div>
           <span class="right floated time">
             <strong>{wishDetails.quantity}</strong>
           </span>
-          <span class="category meta">
-            <i class="database icon" />
+                    <span class="category meta">
+            <i class="database icon"/>
             Quantity
           </span>
+                </div>
+            </div>
+            <div class="extra content">
+                <i class="comments outline icon"/>
+                {offers && offers.length === 1 ? (
+                    <>{offers && offers.length} Offer</>
+                ) : (
+                    <>
+                        {offers && offers.length > 0 ? (
+                            <>{offers && offers.length} Offers</>
+                        ) : (
+                            "No Offers"
+                        )}
+                    </>
+                )}
+            </div>
         </div>
-      </div>
-      <div class="extra content">
-        <i class="comments outline icon" />
-        {offers && offers.length === 1 ? (
-          <>{offers && offers.length} Offer</>
-        ) : (
-          <>
-            {offers && offers.length > 0 ? (
-              <>{offers && offers.length} Offers</>
-            ) : (
-              "No Offers"
-            )}
-          </>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default WishDetailsCardComponent;
