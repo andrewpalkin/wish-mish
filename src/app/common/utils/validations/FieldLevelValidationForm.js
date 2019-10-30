443f5bd0dbece26cb84871a9577298f4e0f97b99
Andrew Palkin <31889705+andrewpalkin@users.noreply.github.com>
2019-10-30 15:24:07 +0200

    Fix of package versions as critical issues

--- src/app/common/utils/validations/FieldLevelValidationForm.js
+++ src/app/common/utils/validations/FieldLevelValidationForm.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const required = value => (value ? undefined : "Required");
 export const number = value =>
     value && isNaN(Number(value)) ? "Must be a number" : undefined;
